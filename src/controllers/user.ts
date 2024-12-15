d476ba74ee830b99e06d1034fd5cf438f9019c81
Adebobola <adepojuadebobola6@gmail.com>
2024-12-15 23:51:57 +0100

    pushing bugs away

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,9 +1,11 @@
+import { Address } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { nextTick } from "process";
 import { prisma } from "../config/database";
 import { AppError } from "../errors/ApiError";
 import { addressSchema } from "../schema/address";
 import { productSchema } from "../schema/product";
+import { updateUserSchema } from "../schema/user";
 
 export const addAddress = async (
   req: Request,
@@ -41,5 +43,43 @@ export const listAddress = async (req: Request, res: Response) => {
 
   res.status(200).json({
     data: addAddress,
+  });
+};
+
+export const updateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const validatedData = updateUserSchema.parse(req.body);
+  let shippingAddress: Address;
+  let billingAddress: Address;
+
+  if (
+    !validatedData.defaultShippingAddress &&
+    !validatedData.defaultBillingAddress
+  ) {
+    return next(new AppError("billing amd shipping address empty", 400));
+  }
+
+  shippingAddress = await prisma.address.findFirstOrThrow({
+    where: { id: validatedData.defaultShippingAddress ?? 0 },
+  });
+
+  billingAddress = await prisma.address.findFirstOrThrow({
+    where: { id: validatedData.defaultShippingAddress ?? 0 },
+  });
+
+  if (!shippingAddress && !billingAddress) {
+    return next(new AppError("not address matches this billing id ", 400));
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: { id: req.user.id },
+    data: validatedData,
+  });
+
+  res.status(200).json({
+    updatedUser,
   });
 };
--- src/schema/user.ts
+++ src/schema/user.ts
@@ -5,3 +5,9 @@ export const signUpSchema = z.object({
   email: z.string().email(),
   password: z.string().min(18),
 });
+
+export const updateUserSchema = z.object({
+  name: z.string(),
+  defaultShippingAddress: z.number().nullable(),
+  defaultBillingAddress: z.number().nullable(),
+});
